cf0798c342b2f869c3c720668a7f82e24f1ac95e
anonaba <117132737+anonaba@users.noreply.github.com>
2026-08-17 16:33:32 +0800

    Update SlowComponentTest.jsx

--- 12-atomic-blog/src/SlowComponentTest.jsx
+++ 12-atomic-blog/src/SlowComponentTest.jsx
@@ -13,7 +13,7 @@ function SlowComponent() {
   )
 }
 
-function Counter({ children }) { // Solution SlowComponent Rerendering 
+function Counter({ children }) { // Solution SlowComponent Rerendering 👉 Optimization Trick With children
   const [count, setCount] = useState(0)
   return (
     <div>
@@ -29,6 +29,34 @@ export default function SlowComponentTest() { // call it in List component in Ap
     <Counter>
       <SlowComponent />
     </Counter>
+  )
+}
+
+
+
+
+// the approach above Optimization Trick with children prop can be done also with react memo
+const SlowComponent = memo(function SlowComponent() {
+  const words = Array.from({ length: 100_000 }, () => 'WORD')
+  return (
+    <ul>
+      {words.map((word, i) => (
+        <li key={i}>
+          {i}: {word}
+        </li>
+      ))}
+    </ul>
+  )
+})
+
+export default function SlowComponentTest() { //call it in List component in App.jsx to test
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+      <SlowComponent />
+    </div>
   )
 }
 
